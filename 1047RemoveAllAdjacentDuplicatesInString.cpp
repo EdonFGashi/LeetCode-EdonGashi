5542694b56cff79159bb4889d2e4b30618f204aa
Edon Gashi <edongashi2004@outlook.com>
2023-05-18 13:07:05 +0200

    change3

---
+++ 1047RemoveAllAdjacentDuplicatesInString.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<stack>
+#include<string>
+using namespace std;
+
+  string removeDuplicates(string s) {
+        stack<char> stacki;
+        
+        for (char c : s) {
+            if (!stacki.empty() && stacki.top() == c) {
+                stacki.pop();
+            } else {
+                stacki.push(c);
+            }
+        }
+        
+        string rezultati;
+        while (!stacki.empty()) {
+            rezultati = stacki.top() + rezultati;
+            stacki.pop();
+        }
+        
+        return rezultati;
+    }
+
+int main() {
+    string S = "abbaca";
+    cout << removeDuplicates(S) << endl;
+    return 0;
+}
