e2cd7974fc44c051c710181caaf4750ea9f436e8
Edon Gashi <edongashi2004@outlook.com>
2023-05-18 15:07:42 +0200

    change4

--- 387FirstUniqueCharacterinaString.cpp
+++ 387FirstUniqueCharacterinaString.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int firstUniqChar(string s) {
-        int frekuenca[26] = {0};
+        int frekuenca[26] = {0}; // ne fillim te gjitha shkronjat e kan frekuencen 0, e pastaj saher qe perdoren inkrementohet per nga nje (line 9)
 
         for (char c : s) {
             frekuenca[c - 'a']++;// per fjalen "leetcode" frekuencat jan a = 0, b = 0 , l = 1, e = 2...
